afe20cdb113150e6b71e3157712258c8d9fc9599
MaximMarinov <maximmarinovv@gmail.com>
2022-05-21 20:47:43 +0300

    Setup; Added Core Module; Header component

---
+++ game-catalog/src/app/core/core.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { SearchBarComponent } from './header/search-bar/search-bar.component';
+
+
+
+@NgModule({
+  declarations: [
+    HeaderComponent,
+    SearchBarComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    HeaderComponent
+  ],
+})
+export class CoreModule { }
